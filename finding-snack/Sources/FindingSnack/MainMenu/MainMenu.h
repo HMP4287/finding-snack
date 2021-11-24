771628aa45202bad7e2c34a155e4720beef497fc
HMP4287 <hyunmins4287@gmail.com>
2021-11-24 15:50:45 +0900

    수정된 것 안올라간 것

--- finding-snack/Sources/FindingSnack/MainMenu/MainMenu.h
+++ finding-snack/Sources/FindingSnack/MainMenu/MainMenu.h
@@ -2,8 +2,23 @@
 
 class MainMenu {
 public: 
+	FindingSnack* findingSnackGame;
+	ScenePtr MainMenuScene; 
 
-	MainMenu() {
-		
+	MainMenu(FindingSnack * findingSnack) {
+		findingSnackGame = findingSnack;
 	};
+
+	// 모든 함수에서 공통적으로 사용할 함수들 
+/*
+start();
+Enter();
+Exit();
+Update(int dt);
+Draw();
+Event(void event);
+getScene();*/
+
+
+	
 };
