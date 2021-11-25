adacc40b8e4470844c43d47c26732002237ab112
HMP4287 <hyunmins4287@gmail.com>
2021-11-26 00:20:01 +0900

    구조 완성

--- finding-snack/Sources/Main.cpp
+++ finding-snack/Sources/Main.cpp
@@ -1,78 +1,20 @@
-﻿#include <iostream>
-//#include "FindingSnack/FindingSnack.h"
+﻿#include "FindingSnack/FindingSnack.h"
 
-//#include "Test/Waiter.h" // 필수 
-//#include "Test/HawaiianPizzaBuilder.h" // 필수 
-//#include "Test/SpicyPizzaBuilder.h" // 필수 
-//#include "Test/Pizza.h"
-//#include "Test/PizzaBuilder.h"
-//
-//
-//#include "FindingSnackTest/Builder.h"
-//#include "FindingSnackTest/MainMenuSceneBuilder.h"
-//#include "FindingSnackTest/FindingSnack.h"
-
-//#include "FindingSnackLast/FindingSnackTest.h"
-
-#include "FindingSnackLast/Builder.h"
-#include "FindingSnackLast/MainMenuSceneBuilder.h"
+// FindingSnack을 개발하기 위해서 필요한 Components 들 (부품 느낌)
+#include "FindingSnack/MainMenuComponent/MainMenuComponent.h"
 
 
 int main()
 {
 
-    // 파일의 생성을 순서대로 생각해봤을 때 상속을 포기하거나, (MainMenu가 생성 할 때 FindingSnack은 컴퓨터에 아무런 정보가 없는 상태) 
-    // 다른파일로 분리하는 것을 포기하던가. 
-    // 따라서 -> 상속을 포기하고, 기존 목적인 객체지향적 설계에서 가독성을 위한 설계로 변경 
-    // MainMenu 클래스와 findingSnack클래스에서 서로에게 접근할 수 있어야 하기 때문에 아래와 같이 설계. 
-    // 더 나은 방안이 있을 수 있음 
-    //FindingSnack findingSnackGame; 
+    FindingSnack findingSnackGame;
 
-    //Builder builder; 
-    //MainMenuSceneBuilder mainMenuSceneBuilder;
-    //builder.SetSceneBuilder(&mainMenuSceneBuilder);
-    //builder.constructScene();
-    ////ScenePtr main = builder.getScene();
-    //findingSnackGame.setMainMenuScene(builder.getScene());
-    //findingSnackGame.start();
-    FindingSnack root;
+    // FindingSnackGame을 개발하기 위해 필요한 씬들을 각각 Components 안에서 생성하여 findingSnackGame 최종적으로 개발.
+    // 저장은 안해도 될 듯함. 안되면 1. main함수에서 처리 -> 2. 저장할 곳 생각하기. (상호참조 되지 않게 고려 필요)
 
-    MainMenuSceneBuilder mainMenuSceneBuilder(&root); // 생성자에서 construct 실행 ?
-    // Select. .  .. 같은것도 다 마찬가지. 
+    MainMenuComponent mainMenuSceneBuilder(&findingSnackGame);
 
-    root.start();
+    findingSnackGame.start();
     
-
-
-    //Builder builder; //root 만 박혀있는 상태 
-    //builder.setBuilder(&root);
-
-
-    //MainMenuSceneBuilder mainMenuSceneBuilder(&root);
-    //builder.construct();
-
-    // 빌더 생성시 findingSnack의 생성자를 한번 호출함 
-    //builder.setBuilder();
-
-
-
-
-
-    //Waiter waiter;
-    //HawaiianPizzaBuilder hawaiianPizzaBuilder;
-    //waiter.SetPizzaBuilder(&hawaiianPizzaBuilder);
-    //waiter.ConstructPizza();
-
-    //std::auto_ptr<Pizza> pizza = waiter.GetPizza();
-    //pizza->ShowPizza();
-    //SpicyPizzaBuilder spicyPizzaBuilder;
-    //waiter.SetPizzaBuilder(&spicyPizzaBuilder);
-    //waiter.ConstructPizza();
-    //pizza = waiter.GetPizza();
-    //pizza->ShowPizza();
-
-
-
-
     return 0; 
 }
